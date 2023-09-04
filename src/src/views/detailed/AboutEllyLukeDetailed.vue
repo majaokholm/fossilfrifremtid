a0335b578ac6b8e40b8956205c860e5708c4b250
Jon Poulsen <jnpoulsen@live.dk>
2023-09-04 22:21:47 +0200

    Replace views in App.vue with RouterView to enable subpaging

---
+++ src/src/views/detailed/AboutEllyLukeDetailed.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="view-container container-bg">
+      <div>
+      <h1>Elly-Luke</h1>
+        Elly-Luke felterne ligger i Nordsøen og dækker tilsammen 160 km2. 
+        Energistyrelsen har vurderet, at Elly-Luke felterne indeholder 5.2 mia. kubikmeter gas. 
+        Det svarer til 2.5 gange Danmarks årlige gasforbrug og vil, hvis det bliver brændt, udlede 10.6 mio. ton CO2. 
+        Det vil betyde en 50 % øgning af gasproduktionen i Danmark. 
+        Regeringen kalder Elly-Luke projektet for et “mini-udbud”. Faktum er, at der ikke er noget ”mini” over denne udbudsrunde. Åbningen af dette felt vil få katastrofalt store konsekvenser.
+        Elly-Luke-feltet er også et kendt risikabelt projekt, i forhold til hvor rentabelt det ville være at udvinde gas og olie. Flere selskaber har opgivet området af økonomiske årsager. 
+        Desuden vil den energi, der vil blive brugt på at udvinde gas fra området, udlede 400.000 ton CO2. 
+        Vi har ikke råd til at indvinde ny fossil energi. Hverken økonomisk eller klimamæssigt.
+        Fossilfri Fremtid arbejder for at stoppe Elly-Luke projektet fra at blive en realitet og stoppe alle fremtidige fossile projekter i Nordsøen. 
+        Læs ministeriets egen tekniske gennemgang af udbudsrunden her. 
+      <!--Indsæt læs-mere-knap her--> 
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .container-bg{
+        background-color:#FFB48E;
+      }
+  </style>
