b1ed746da986fa6a23efed277d28d6a12bf92f81
KMOQ <KMOQ@novonordisk.com>
2023-09-10 20:43:28 +0200

    fix nav bug

--- src/src/views/HomeView.vue
+++ src/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AboutEllyLukeView from '../views/AboutEllyLukeView.vue'
 import AboutFossilfriFremtidView from '../views/AboutFossilfriFremtidView.vue'
+import HvemStaarBagView from '../views/HvemStaarBag.vue'
 import ActivityView from '../views/ActivityView.vue'
 import ContactView from '../views/ContactView.vue'
 import TopView from '../views/TopView.vue'
@@ -11,6 +12,7 @@ import TopView from '../views/TopView.vue'
     <TopView id="top"></TopView>
     <AboutFossilfriFremtidView id="om-fossilfri-fremtid"></AboutFossilfriFremtidView>
     <AboutEllyLukeView id="om-elly-luke"></AboutEllyLukeView>
+    <HvemStaarBagView id="hvem-staar-bag"></HvemStaarBagView>
     <ActivityView id="aktiviteter"></ActivityView>
     <ContactView id="kontakt"></ContactView>
   </main>
