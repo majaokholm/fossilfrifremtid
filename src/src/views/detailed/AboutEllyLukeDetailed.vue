cab14e6b92fec4e6c3422e167844c35d48634799
Thomas <thomas1nicolet@gmail.com>
2023-09-18 14:03:39 +0200

    bug fix: add image code to view page as well

--- src/src/views/detailed/AboutEllyLukeDetailed.vue
+++ src/src/views/detailed/AboutEllyLukeDetailed.vue
@@ -3,35 +3,79 @@
 </script>
 
 <template>
-    <div class="view-container container-bg">
-      <div>
-      <h1>Hvad er Elly-Luke?</h1>
-
-      <br>
-      <p>Elly-Luke felterne ligger i Nordsøen og dækker tilsammen 160 km2. 
-      Energistyrelsen har vurderet, at Elly-Luke felterne indeholder 5.2 mia. kubikmeter gas. 
-      Det svarer til 2.5 gange Danmarks årlige gasforbrug og vil, hvis det bliver brændt, udlede 10.6 mio. ton CO2. 
-      Det vil betyde en 50 % øgning af gasproduktionen i Danmark. </p>
-
-      <br>
-      <p>Regeringen kalder Elly-Luke projektet for et “mini-udbud”. Faktum er, at der ikke er noget ”mini” over denne udbudsrunde. Åbningen af dette felt vil få katastrofalt store konsekvenser.</p>
-      
-      <br>
-      <p>Elly-Luke-feltet er også et kendt risikabelt projekt, i forhold til hvor rentabelt det ville være at udvinde gas og olie. Flere selskaber har opgivet området af økonomiske årsager. 
-      Desuden vil den energi, der vil blive brugt på at udvinde gas fra området, udlede 400.000 ton CO2. </p>
-      
-      <br>
-      <p>Vi har ikke råd til at indvinde ny fossil energi. Hverken økonomisk eller klimamæssigt.
-      Fossilfri Fremtid arbejder for at stoppe Elly-Luke projektet fra at blive en realitet og stoppe alle fremtidige fossile projekter i Nordsøen. 
-      Læs ministeriets egen tekniske gennemgang af udbudsrunden <a href="https://www.ft.dk/samling/20222/almdel/KEF/bilag/261/2696582.pdf">her</a>.
-
-    </p>
+  <div class="view-container container-bg">
+    <div class="content">
+      <!-- Text on the left -->
+      <div class="text">
+        <h1>Hvad er Elly-Luke?</h1>
+        <br>
+        <p>Elly-Luke felterne ligger i Nordsøen og dækker tilsammen 160 km2. 
+        Energistyrelsen har vurderet, at Elly-Luke felterne indeholder 5.2 mia. kubikmeter gas. 
+        Det svarer til 2.5 gange Danmarks årlige gasforbrug og vil, hvis det bliver brændt, udlede 10.6 mio. ton CO2. 
+        Det vil betyde en 50 % øgning af gasproduktionen i Danmark.</p>
+        <br>
+        <p>Regeringen kalder Elly-Luke projektet for et “mini-udbud”. Faktum er, at der ikke er noget ”mini” over denne udbudsrunde. Åbningen af dette felt vil få katastrofalt store konsekvenser.</p>
+        <br>
+        <p>Elly-Luke-feltet er også et kendt risikabelt projekt, i forhold til hvor rentabelt det ville være at udvinde gas og olie. Flere selskaber har opgivet området af økonomiske årsager. 
+        Desuden vil den energi, der vil blive brugt på at udvinde gas fra området, udlede 400.000 ton CO2.</p>
+        <br>
+        <p>Vi har ikke råd til at indvinde ny fossil energi. Hverken økonomisk eller klimamæssigt.
+        Fossilfri Fremtid arbejder for at stoppe Elly-Luke projektet fra at blive en realitet og stoppe alle fremtidige fossile projekter i Nordsøen. 
+        Læs ministeriets egen tekniske gennemgang af udbudsrunden <a href="https://www.ft.dk/samling/20222/almdel/KEF/bilag/261/2696582.pdf">her</a>.
+        </p>
+      </div>
+      <!-- Image on the right -->
+      <div class="image">
+        <img src="@/assets/ellyluke.png" alt="Image Alt Text">
+        <!-- Image Description Text -->
+        <p class="image-description">Eksisterende olie- og gasfelter (blå) og Elly-Luke (rød). Kilde: <a href = "https://ens.dk/presse/miniudbud-til-efterforskning-og-indvinding-i-et-afgraenset-omraade-i-nordsoeen">ens.dk</a> </p>
+       
       </div>
     </div>
+  </div>
 </template>
-  
-  <style scoped>
-  .container-bg{
-        background-color:#FFF8D8;
-      }
-  </style>
+
+<style scoped>
+.container-bg {
+  background-color: #FFF8D8;
+}
+
+.content {
+  display: flex;
+  flex-direction: row; /* Keep the order of text and image */
+  align-items: center; /* Vertically center content */
+}
+
+.text {
+  flex: 1; /* Allow the text to take up remaining space */
+}
+
+.image {
+  margin-left: 20px; /* Adjust the margin to create space between text and image */
+  display: flex; /* Enable flex layout for the image and description */
+  flex-direction: column; /* Stack image and description vertically */
+  align-items: center; /* Center items horizontally */
+}
+
+.image img {
+  max-width: 100%; /* Ensure the image doesn't exceed its container width */
+}
+
+.image-description {
+  font-size: 14px; /* Adjust the font size as needed */
+  margin-top: 10px; /* Add top margin for spacing */
+  text-align: center; /* Align the text in the center */
+}
+
+/* Additional styling for responsiveness if needed */
+@media (max-width: 768px) {
+  .content {
+    flex-direction: column; /* Change to a vertical layout on smaller screens */
+  }
+
+  .image {
+    margin-left: 0; /* Reset the margin for vertical layout */
+    margin-top: 20px; /* Add top margin for spacing */
+  }
+}
+</style>
