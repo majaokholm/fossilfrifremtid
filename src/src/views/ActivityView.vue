29cdad264cefafd4a5d6229b52b5360f9b09d596
Thomas <thomas1nicolet@gmail.com>
2023-09-18 14:34:11 +0200

    change button color grey to orange

--- src/src/views/ActivityView.vue
+++ src/src/views/ActivityView.vue
@@ -12,51 +12,48 @@
 
       <div class="button-container">
         <RouterLink to="aktiviteter-borger">
-          <button href="" class="read-more-button">BORGER</button>
+          <button href="" class="read-more-button orange-button">BORGER</button>
         </RouterLink>
 
         <RouterLink to="aktiviteter-virksomhed">
-          <button href="" class="read-more-button">VIRKSOMHED</button>
+          <button href="" class="read-more-button orange-button">VIRKSOMHED</button>
         </RouterLink>
 
         <RouterLink to="aktiviteter-organisation">
-          <button href="" class="read-more-button">ORGANISATION</button>
+          <button href="" class="read-more-button orange-button">ORGANISATION</button>
         </RouterLink>
       </div> 
-      <!-- <p>Skriv en mail til <a href = "mailto: fossilfrifremtid@gmail.com">fossilfrifremtid@gmail.com</a> eller send en sms til 31 52 31 21. 
-      </p>
-      <br>
-      <p>Vores styrke er, at vi er mange og at vi kommer med forskellige baggrunde. Der er brug for dig, lige meget om du har deltaget i en kampagne før, eller om du er helt grøn. Om du har lidt eller meget tid. Lige meget hvor du bor, din alder, etnicitet, seksualitet eller politiske overbevisning. </p>
-      <br>
-      <p>Kampagnen er familievenlig, hvilket betyder at vi tilstræber familievenlige aktiviteter, møder udenfor puttetid og børnepasning til fysiske møder. </p>
-      <br>
-      <p>Vi organiserer os i selvkørende arbejdsgrupper, som blandt andet tager sig af at lave kreative projekter, flotte videoer og billeder, snakke med politikere og erhvervsliv, sociale medier, holde hyggelige og effektive møder og meget mere. Der er altid plads til flere i arbejdsgrupperne.</p> -->
     </div>
   </div>
 </template>
 
-
 <style scoped>
-.container-bg{
-      background-color:#FFF8D8;
-    }
-.button-container {
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      width:100%;
-    }
+.container-bg {
+  background-color: #FFF8D8;
+}
 
-    .read-more-button{
-      border: 2px solid black;
-      border-radius: 2em;
-      margin-bottom: 0.5em;
-      margin-top: 0.5em;
-      color: black;
-      cursor: pointer;
-      font-size: 1.25em;
-      font-weight: bold;
-      flex-wrap: nowrap;
-      padding: 1em;
-    }
+.button-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+}
+
+.read-more-button {
+  border: 2px solid black;
+  border-radius: 2em;
+  margin-bottom: 0.5em;
+  margin-top: 0.5em;
+  color: black;
+  cursor: pointer;
+  font-size: 1.25em;
+  font-weight: bold;
+  flex-wrap: nowrap;
+  padding: 1em;
+}
+
+/* Define the orange-button class */
+.orange-button {
+  background-color: #FFB48E; /* Set the background color to orange */
+}
 </style>
