597f88c1b1ee7ac09dfec55f837d8b0dd8532e7e
Jon Poulsen <jnpoulsen@live.dk>
2023-11-13 22:08:03 +0100

    text about Nordsoeaftalen + placeholder images on info subpages

--- src/src/views/detailed/AboutHejreDetailed.vue
+++ src/src/views/detailed/AboutHejreDetailed.vue
@@ -13,6 +13,9 @@ Da INEOS nu ejer nabofeltet, kan udbygningen af Hejrefeltet gøres simplere end 
 INEOS sendte en ansøgning med deres planer for Hejre til Energistyrelsen d. 23. december 2022. Projektet miljøkonsekvensrapport, som er udarbejdet af COWI, blev sendt i offentlig høring med deadline 1. september 2023. (13. oktober 2023 meddeler Energistyrelsen 7 års forlængelse af licenserne i Hejrefeltet til INEOS, mens selve godkendelsen af projektet lader vente på sig).
 </p>
     </div>
+    <div class="image-container">
+      
+    </div>
   </div>
 </template>
 
@@ -20,5 +23,17 @@ INEOS sendte en ansøgning med deres planer for Hejre til Energistyrelsen d. 23.
 
 .container-bg {
   background-color: #FFF8D8;
+}
+
+.text-side-container{
+  flex-basis: 75%;
+}
+
+.image-container{
+  background-color: rgb(79, 118, 191);
+  background-image: url('@/assets/FossilfriFremtid_logo2_blå_transparent.png');
+  background-position: center;
+  background-size: 50%;
+  background-repeat: no-repeat;
 }
 </style>
--- src/src/views/detailed/AboutNordsoeaftalenDetailed.vue
+++ src/src/views/detailed/AboutNordsoeaftalenDetailed.vue
@@ -5,8 +5,29 @@
 <template>
   <div class="view-container container-bg">
     <div class="text-side-container" >
-<h1>Historie</h1>
-      <p>(kommer snart)</p>
+<h1>Nordsøaftalen</h1>
+      <p><i>Aftaleparterne er enige om, at Danmark skal tage lederskab for den grønne omstilling og inspirere andre
+lande til klimahandling. Derfor aflyser vi 8. udbudsrunde og sætter samtidigt en slutdato for olie- og gasindvinding i Nordsøen i 2050.</i><br><br>
+
+Så lovende formuleres det i starten af Nordsøaftalen fra 2020, der blev vedtaget af et bredt flertal i Folketinget. Længere nede i teksten bliver det endnu bedre:<br><br>
+<i>Aftaleparterne er enige om, at 8. udbudsrunde aflyses, og at der ikke skal være flere udbudsrunder i fremtiden.</i><br><br>
+
+Men sætningen efter går det galt:<br><br>
+<i>Det betyder, at ny efterforskning fremover udelukkende sker igennem de allerede eksisterende muligheder i minirunder og naboblok, så det ikke længere er staten, der inviterer til efterforskning efter fossile
+brændsler.</i><br><br>
+
+Sagt med andre ord: Hvis en virksomhed uopfordret ansøger om udvinding i Nordsøen, kan staten stadig oprette et udbud. Det var det, der skete, 
+da BlueNord i 2023 ansøgte om udvinding i gasfeltet Elly-Luke. Selskabet endte med at trække sin ansøgning, men smuthullet i Nordsøaftalen er der stadig, 
+klar til at blive udnyttet af næste ansøger. Derfor kæmper Fossilfri Fremtid for, at Nordsøaftalen genforhandles, så vi undgår et kapløb om at hente flest mulige fossile brændsler op inden deadline i 2050.<br><br>
+
+<a target="_blank" href="https://kefm.dk/Media/0/3/Nords%C3%B8aftale%20(2).pdf">Link til aftaleteksten.</a>
+
+
+
+</p>
+    </div>
+    <div class="image-container">
+      
     </div>
   </div>
 </template>
@@ -15,5 +36,17 @@
 
 .container-bg {
   background-color: #FFF8D8;
+}
+
+.text-side-container{
+  flex-basis: 75%;
+}
+
+.image-container{
+  background-color: rgb(79, 118, 191);
+  background-image: url('@/assets/FossilfriFremtid_logo2_blå_transparent.png');
+  background-position: center;
+  background-size: 50%;
+  background-repeat: no-repeat;
 }
 </style>
