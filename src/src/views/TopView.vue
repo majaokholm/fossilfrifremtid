596264645c375b4f0ecb87cdf7c7856e97758083
KMOQ <KMOQ@novonordisk.com>
2024-01-17 15:13:31 +0100

    css clean up and changes for stop hejre campaign

--- src/src/views/TopView.vue
+++ src/src/views/TopView.vue
@@ -1,33 +1,57 @@
 <template>
   <div class="view-container">
-    <div class="filter-container">
-    <div class="content-container">
-      <div class="text-container">
-        <h1 class="white-text large-text">Ikke flere olie- og gasfelter i Nordsøen</h1>
-        <h1 class="white-text large-text">Vi stoppede Elly-Luke</h1>
-        <h1 class="white-text large-text">Nu stopper vi Hejre</h1>
-      </div>
 
-      <!-- <RouterLink to="om-hejre">
-            <a href="/om-hejre" class="read-more-link">Luk oliefeltet Hejre</a>
-          </RouterLink><br> -->
+    <div class="text-container">
+      <h1 class="white-text large-text">Ikke flere olie- og gasfelter i Nordsøen</h1>
+      <h1 class="white-text large-text">Stop Hejre-feltet</h1>
+      <h1 class="white-text large-text">Hold din del af aftalen, Mette</h1>
+    </div>
 
-    <RouterLink to="om-hejre">
-      <div class="button-container">
-        <v-btn
-          class="tall-button"
-          color="yellow darken-2"
-          dark
-          href="/om-hejre"
-          rel="noopener noreferrer"
-          size="x-large"
-        >
-          <span style="color: black;">LÆS MERE OM HEJREFELTET</span>
-        </v-btn>   
-      </div>
-    </RouterLink>
-    </div>
-    </div>
+    <div class="button-container">      
+      <v-btn
+        class="tall-button"
+        color="yellow darken-2"
+        dark
+        href="/om-hejre"
+        rel="noopener noreferrer"
+        size="medium"
+      >
+        <span style="color: black;">Fakta om Hejrefeltet</span>
+      </v-btn>
+      <v-btn
+        class="tall-button"
+        color="yellow darken-2"
+        dark
+        href="https://www.dr.dk/nyheder/viden/klima/danmark-kraever-stop-olie-og-gas-paa-klimatopmoedet-men-vi-slukker-ikke-vores"
+        target="_blank"
+        rel="noopener noreferrer"
+        size="medium"
+      >
+        <span style="color: black;">ARTIKEL OM HEJREFELTET PÅ DR.DK</span>
+      </v-btn>  
+      <v-btn
+        class="tall-button"
+        color="yellow darken-2"
+        dark
+        href="https://www.dgub.dk/blog/der-er-et-nyt-stort-oliefelt-pa-vej-i-nordsoen-og-det-skal-stoppes"
+        target="_blank"
+        rel="noopener noreferrer"
+        size="medium"
+      >
+        <span style="color: black;">NYT OLIEFELT PÅ VEJ I NORDSØEN</span>
+      </v-btn>  
+      <v-btn
+        class="tall-button"
+        color="yellow darken-2"
+        dark
+        href="https://www.instagram.com/fossilfrifremtid/"
+        target="_blank"
+        rel="noopener noreferrer"
+        size="medium"
+      >
+        <span style="color: black;">FOSSILFRI FREMTID PÅ INSTAGRAM</span>
+      </v-btn>   
+    </div>        
   </div>
 </template>
 
@@ -35,31 +59,17 @@
 .view-container{
   background-image: url('@/assets/frontpage_image_web_shade.jpg');
   background-size: cover;
-  padding: 0;
-  display: block;
-  position: relative;
-}
-
-.filter-container{
-  height: 100%;
-  width: 100%;
   backdrop-filter: brightness(0.5);
-  display: flex;
-  position: absolute;
-}
-
-.content-container {
-  text-align: center;
-  max-width: 1000px;
-  margin: 0 auto;
-  position: relative;
+  min-width: 100%;
+  padding: 0;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  align-items: center;
 }
 
-.text-container {
-  padding: 20px;
+.text-container {  
+  margin-top: 50px;
+  text-align: center;
 }
 
 .text-container .white-text {
@@ -73,10 +83,13 @@
 
 .button-container {
   margin-top: 20px;
+  display: flex;
+  flex-direction: column;
 }
 
 .tall-button {
   height: 60px !important;
+  margin-bottom: 20px;
 }
 
 /* Responsive CSS */
